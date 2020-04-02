697aac1692eeb63f204cf08f5cacc4c0388a9006
papu <papu.datta@gmail.com>
2020-04-02 10:44:35 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"net"
+)
+
+// Obtained these functions from various sources
+
+func main() {
+	http.HandleFunc("/", ReqHandler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
+
+func ReqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	resp, _ := json.Marshal(map[string]string{
+		"your_ip": getip(r), "my_hostname": gethn(), "my_ip": getmyip(),
+	})
+	w.Write(resp)
+}
+
+// GetIP gets a requests IP address by reading off the forwarded-for
+// header (for proxies) and falls back to use the remote address.
+func getip(r *http.Request) string {
+	forwarded := r.Header.Get("X-FORWARDED-FOR")
+	if forwarded != "" {
+		return forwarded
+	}
+	return r.RemoteAddr
+}
+
+func gethn() string {
+	hn, err := os.Hostname()
+	if err != nil {
+		return "There was an error retrieving the hostname"
+		panic(err)
+    }
+	return hn
+}
+
+func getmyip() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// Filter loopback
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
+}
